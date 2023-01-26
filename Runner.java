e39ca712cb40e1852fba363a6bf742f7d015b1b9
c1nnamoroll <treasure89238@students.sbcsc.k12.in.us>
2023-01-26 08:48:20 -0500

    Created starRating method

--- Runner.java
+++ Runner.java
@@ -1,7 +1,7 @@
 public class Runner {
     public static void main(String[] args) {
         System.out.println(Review.sentimentVal("Dog"));
-        System.out.println(Review.sentimentVal("is"));
+        System.out.println(Review.sentimentVal("is` "));
         System.out.println(Review.sentimentVal("terrible"));
 
         System.out.println(Review.totalSentiment("simpleReview.txt"));
